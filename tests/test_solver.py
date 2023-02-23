595a26fc2f433d9abfe98dc4c488c95d4cdefc1c
Gabriel Silva <gls.facom@gmail.com>
2023-02-23 09:20:25 -0400

    improve tests with parametrization

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -1,12 +1,13 @@
 import warnings
 from numpy import cos, sin
 import numpy as np
+import pytest
 
-def plot(solver, error):
+def plot(solver, error, name:str = 'error'):
     import matplotlib.pyplot as plt
     from matplotlib import cm
 
-    fig = plt.figure(num='divergence', figsize=plt.figaspect(0.5))
+    fig = plt.figure(num=name, figsize=plt.figaspect(0.5))
     ax1 = fig.add_subplot(1, 1, 1, projection='3d')
     ax1.set_title('Erro')
     fig.suptitle(f'Norma infito do erro = {max(error):1e}',fontsize=20)
@@ -14,79 +15,26 @@ def plot(solver, error):
     fig.colorbar(surf, ax=ax1, fraction=0.1, pad=0.2)
     plt.show()
 
-def test_divergence(solver):
-    def div_problem(x,y):
-        return -2*cos(x)*cos(y),-2*cos(x)*cos(y)
-
-    def div_solution(x,y):
-        return 2*sin(x+y)
-    
-    solver.vectors = np.asarray([div_problem(p[0],p[1]) for p in solver.mesh.points])
-    div_sol = [div_solution(p[0],p[1]) for p in solver.mesh.points]
-
-    div = np.asarray([solver.divergence(pid) for pid in range(solver.mesh.n_points)])
-
-    error = abs(div-div_sol)
-    
-    assert max(error) <= 10e-3
-
-def test_divergence_free(solver):
-    def divfree_problem(x,y):
-        return x, -y
-
-    def divfree_solution(x,y):
-        return 0
-    
-    solver.vectors = np.asarray([divfree_problem(p[0],p[1]) for p in solver.mesh.points])
-    div_sol = [divfree_solution(p[0],p[1]) for p in solver.mesh.points]
+@pytest.mark.parametrize("problem, solution", [
+    (lambda x, y: (-2*cos(x)*cos(y),-2*cos(x)*cos(y)), lambda x, y: 2*sin(x+y)),
+    (lambda x, y: (x, -y), lambda x, y: 0),
+    (lambda x, y: (cos(y), sin(x)), lambda x, y: 0),
+    (lambda x, y: (cos(y)+x, sin(x)-y), lambda x, y: 0),
+    (lambda x, y: (cos(x**2+y), -2*x*cos(x**2+y)), lambda x, y: 0)
+])
+def test_divergence(solver, problem, solution):    
+    solver.vectors = np.asarray([problem(p[0],p[1]) for p in solver.mesh.points])
+    div_sol = [solution(p[0],p[1]) for p in solver.mesh.points]
 
     div = np.asarray([solver.divergence(pid) for pid in range(solver.mesh.n_points)])
 
     error = abs(div-div_sol)
-    
-    assert max(error) <= 10e-3
-
-def test_divergence_free(solver):
-    def divfree_problem(x,y):
-        return cos(y), sin(x)
-    
-    solver.vectors = np.asarray([divfree_problem(p[0],p[1]) for p in solver.mesh.points])
-
-    div = np.asarray([solver.divergence(pid) for pid in range(solver.mesh.n_points)])
-
-    error = abs(div)
-    
-    assert max(error) <= 10e-3
-
-def test_divergence_free_2(solver):
-    def divfree_problem(x,y):
-        return cos(y)+x, sin(x)-y
-    
-    solver.vectors = np.asarray([divfree_problem(p[0],p[1]) for p in solver.mesh.points])
-
-    div = np.asarray([solver.divergence(pid) for pid in range(solver.mesh.n_points)])
-
-    error = abs(div)
-    
-    assert max(error) <= 10e-3
-
-def test_divergence_free_3(solver):
-    def divfree_problem(x,y):
-        return cos(x**2+y), -2*x*cos(x**2+y)
-    
-    solver.vectors = np.asarray([divfree_problem(p[0],p[1]) for p in solver.mesh.points])
-
-    div = np.asarray([solver.divergence(pid) for pid in range(solver.mesh.n_points)])
-
-    error = abs(div)
     
     try:
         assert max(error) <= 10e-3
     except:
-        plot(solver, error)
+        plot(solver, error, "divergence")
         assert max(error) <= 10e-3
-
-
 
 def test_gradient(solver):
     def grad_problem(x,y):
